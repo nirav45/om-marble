91be7dcb35e4a4aa0ff307ab57bcfd09bdc1bd36
nirav45 <nirav1549@gmail.com>
2020-12-11 22:21:45 +0530

    css path changes

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -5,22 +5,22 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css') }}">
-        <link rel="stylesheet" type="text/css" href=" {{ asset('css/animate.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/aos.css') }}" >
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="/css/all.min.css">
+        <link rel="stylesheet" href="/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="/css/owl.theme.default.min.css">
+        <link rel="stylesheet" href="/css/fontawesome.min.css">
+        <link rel="stylesheet" type="text/css" href=" /css/animate.css">
+        <link rel="stylesheet" href="/css/aos.css" >
 
-        <script src="{{ asset('js/javascript.js') }}"></script>
-        <script src="{{ asset('js/jquery.min.js') }}"></script>
-        <script src="{{ asset('js/popper.min.js') }}"></script>
-        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('js/aos.js') }}"></script>
-        <script type="text/javascript" src="{{ asset('js/wow.min.js') }}"></script>
-        <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+        <script src="{/js/javascript.js"></script>
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/popper.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/aos.js"></script>
+        <script type="text/javascript" src="/js/wow.min.js"></script>
+        <script src="/js/owl.carousel.min.js"></script>
 
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900">
     </head>
@@ -28,6 +28,6 @@
         @include('../includes/header');
         @yield('content')
         @include('../includes/footer');
-        <script src="{{ asset('js/myScript.js') }}"></script>
+        <script src="/js/myScript.js"></script>
     </body>
 </html>
