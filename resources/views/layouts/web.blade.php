4016d1c30749abaa7129a68e2eaab55b7460a450
Hiten <hitenbhayani41@gmail.com>
2020-12-13 19:32:23 +0530

    minor change

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Om Marbles - @yield('title')</title>
+        <title>@yield('title') | Om Marbles</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
